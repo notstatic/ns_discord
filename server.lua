be55e65f15d0286527c45c3f5c92549cdfe3493f
notstatic <75686734+notstatic@users.noreply.github.com>
2020-12-17 12:27:32 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,14 +1,36 @@
+-- Config --
+local discord = true -- vaaditaanko discordia, jotta palvelimelle pääsee
+local xbl = true -- vaaditaanko xbox liveä, jotta palvelimelle pääsee
+local live = true -- vaaditaanko LiveID:tä, jotta palvelimelle pääsee
+-- Koodi --
+
 AddEventHandler("playerConnecting", function (u, skr, df)
     local s = source
     local i = ExtractIdentifiers(s)
-
     df.defer()
-    df.update("Tarkistetaan discord yhteyttä.")
-    if i.d then
-        df.done()
-    else
-        df.done('Palvelin ei tunnistanut discord käyttäjääsi')
+    df.update("Tarkistetaan tunnisteita..")
+    if discord == true then
+        if i.d ~= "" then
+            df.update('discord löytyi')
+        else
+            df.done('Onko discord käynnissä?')
+        end
     end
+    if xbl == true then
+        if i.x ~= "" then
+            df.update('Xbox Live löytyi')
+        else
+            df.done('Onko xbox live linkattu fivemiin?')
+        end
+    end
+    if live == true then
+        if i.lv ~= "" then
+            df.update('LiveID löytyi')
+        else
+            df.done('Onko LiveID linkattu fivemiin?')
+        end
+    end
+    df.done()
 end)
 
 function ExtractIdentifiers(src)
